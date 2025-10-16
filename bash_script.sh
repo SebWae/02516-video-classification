ff08d80c4115f391ca8df1b6ab680ac7fcfe7924
SebWae <s254120@dtu.dk>
2025-10-16 20:26:51 +0200

    increased wall-clock time to 8 hours

--- bash_script.sh
+++ bash_script.sh
@@ -4,7 +4,7 @@
 ### ------------- specify gpu request----------------
 #BSUB -gpu "num=1:mode=exclusive_process"
 ### ------------- specify job name ----------------
-#BSUB -J testjob
+#BSUB -J first_single_frame_run
 ### ------------- specify number of cores ----------------
 #BSUB -n 4
 #BSUB -R "span[hosts=1]"
@@ -12,10 +12,10 @@
 #BSUB -R "rusage[mem=20GB]"
 
 ### ------------- specify wall-clock time (max allowed is 12:00)---------------- 
-#BSUB -W 03:00
+#BSUB -W 08:00
 
 #BSUB -o OUTPUT_FILE%J.out
 #BSUB -e OUTPUT_FILE%J.err
 
 source venv_proj2/bin/activate
-python test_script.py
+python single_frame.py
